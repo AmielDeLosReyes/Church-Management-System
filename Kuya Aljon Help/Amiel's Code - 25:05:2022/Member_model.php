aefcbca785093506b9598fc31c74f60239f3db23
Amiel De Los Reyes <89047257+AmielDeLosReyes@users.noreply.github.com>
2022-05-25 20:49:09 -0600

    Add files via upload

---
+++ Kuya Aljon Help/Amiel's Code - 25:05:2022/Member_model.php
@@ -0,0 +1,82 @@
+<?php
+
+class Member_model extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    public function GetMembers() {
+        $sql = "SELECT * FROM ci_member ORDER BY LAST_NAME ASC";
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        return $result;
+    }
+
+    /**
+     * Grab Ministries data
+     */
+    public function GetMinistries() {
+        $sql = "SELECT * FROM ci_ministry ORDER BY ABBREVIATION ASC";
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        return $result;
+    }
+
+    /**
+     * 
+     * @param type $memberId
+     * @return type
+     */
+    public function GetMember($memberId) {
+        $sql = "SELECT * FROM ci_member WHERE MEMBER_ID = '$memberId'";
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        return $result;
+    }
+
+    /**
+     * Grab one Ministry data
+     */
+    public function GetMinistry($ministryId) {
+        $sql = "SELECT * FROM ci_ministry WHERE MINISTRY_ID = '$ministryId'";
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        return $result;
+    }
+    // function to save members that were edited 
+    public function saveMember($memberId, $data) {
+        // The correct syntax for update in the sql
+        $this->db->where('MEMBER_ID', $memberId);
+        $this->db->update('ci_member', $data);
+    }
+
+    // function to add members that were edited 
+    public function addMember($data) {
+
+        // The correct syntax for update in the sql
+        $this->db->insert('ci_member', $data);
+    }
+
+    // function to delete member/s 
+    public function deleteMember($memberId) {
+        // The correct syntax for delete in the sql
+        $this->db->where('MEMBER_ID', $memberId);
+        $this->db->delete('ci_member');
+    }
+
+    // function to add members that were edited 
+    public function addToMinistry($ministryId, $data) {
+
+        // The correct syntax for update in the sql
+        $this->db->where('MINISTRY_ID', $ministryId);
+        $this->db->insert('ci_ministry_mem', $data);
+    }
+
+}
+
+?>
---
+++ Kuya Aljon Help/Amiel's Code - 25:05:2022/Ministry_model.php
@@ -0,0 +1,65 @@
+<?php
+
+class Ministry_model extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * This function is used to add the event information in the database.
+     * @param type $data
+     */
+    public function addEvent($data) {
+        // The correct syntax for update in the sql
+        $this->db->insert('ci_event', $data);
+        return $this->db->insert_id();
+    }
+    
+    public function updateEvent($eventId, $data) {
+        // The correct syntax for update in the sql
+        $this->db->where('EVENT_ID', $eventId);
+        $this->db->update('ci_event', $data);
+    } 
+    /**
+     * 
+     * @return type
+     */
+    public function GetMinistries() {
+        $sql = "SELECT * FROM ci_ministry ORDER BY ABBREVIATION ASC";
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        return $result;
+    }
+
+    /**
+     * 
+     * @param type $memberId
+     * @return type
+     */
+    public function GetMinistry($ministryId) {
+        $sql = "SELECT * FROM ci_ministry WHERE MINISTRY_ID = '$ministryId'";
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        return $result;
+    }
+
+    // function to save members that were edited 
+    public function saveMinistry($ministryId, $data) {
+        // The correct syntax for update in the sql
+        $this->db->where('MINISTRY_ID', $ministryId);
+        $this->db->update('ci_ministry', $data);
+    }
+
+    // function to add members that were edited 
+    public function addToMinistry($ministryId, $data) {
+
+        // The correct syntax for update in the sql
+        $this->db->where('MINISTRY_ID', $ministryId);
+        $this->db->insert('ci_ministry_mem', $data);
+    }
+
+}
+
+
+?>
